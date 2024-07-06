99dff832eba6c2e48840d9213ff2804b3f17f42c
unknown <ielyse119@gmail.com>
2024-07-06 20:08:34 +0200

    useOptimistic

--- src/components/TestuseActionState.jsx
+++ src/components/TestuseActionState.jsx
@@ -1,25 +1,32 @@
-import { useActionState } from 'react'
+import { useActionState, useOptimistic } from 'react'
 
 const TestuseActionState = () => {
 
   const changeName = async(currentState, formData) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    const newName = formData.get("name");
-    currentState.name = newName;
-
-    return currentState
+    setOptimisticName(formData.get('name'))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      const newName = formData.get("name");
+      currentState.name = newName;
+  
+      return currentState
+    } catch (error) {
+      console.log(error)
+    }
   };
 
   const [state, formAction, isPending] = useActionState(changeName, {
     name : 'elise'
   })
 
+  const [optimisticName, setOptimisticName] = useOptimistic(state.name)
+
   return (
     <div>
       <h2>Test useActionState Hook</h2>
-      <p>Current User: {isPending ? 'loading...' : state.name}</p>
+      <p>Current User: : {optimisticName}   {isPending && "loading..."}</p>
       <form action={formAction}>
-        <input type="text" name='name' placeholder='Enter name...' />
+        <input type="text" name="name" placeholder="Enter name..." />
         <button>Update</button>
       </form>
     </div>
